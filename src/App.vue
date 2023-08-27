2ec78db111a0c695c27ecb5164d239a82f183c75
preska <wolfwar@outlook.com>
2023-08-27 19:19:33 +0200

    mixes inspired by...

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from "vue-router";
 import Magnify from "vue-material-design-icons/Magnify.vue";
 import Bell from "vue-material-design-icons/Bell.vue";
+import SideMenuItem from "./components/SideMenuItem.vue";
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,154 @@
-<script setup></script>
+<script setup>
+import MixesInspiredBy from "../components/MixesInspiredBy.vue";
+// import MultiArtistSelect from "../components/MultiArtistSelect.vue";
+// import CustomCarousel from "../components/CustomCarousel.vue";
+// import Highlights from "../components/Highlights.vue";
+</script>
 
-<template>home view</template>
+<template>
+  <div class="max-w-[1500px] mx-auto">
+    <div class="px-8 mt-8 min-w-[800px] w-full">
+      <div class="text-white text-xl font-semibold inline-block">
+        Mixes inspired by...
+        <div class="text-sm font-light text-[#A2A2AD]">
+          Discover new tracks similar to your favourites
+        </div>
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex justify-between w-full gap-8">
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="VAMPIRE (feat. Oli Sykes of Bring Me The Horizon)"
+            by="Ic3peak"
+            to="/artist"
+            image="https://picsum.photos/id/147/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Deutchland"
+            by="Rammstein"
+            to="/artist"
+            image="https://picsum.photos/id/166/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Fallout"
+            by="Masked Wolf"
+            to="/artist"
+            image="https://picsum.photos/id/127/300/300"
+          />
+        </div>
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Pray For Plagues"
+            by="Bring Me the Horizon"
+            to="/artist"
+            image="https://picsum.photos/id/201/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Candy Truck / You Expected"
+            by="Bring Me the Horizon"
+            to="/artist"
+            image="https://picsum.photos/id/222/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="L'efant sauvage"
+            by="Gojira"
+            to="/artist"
+            image="https://picsum.photos/id/212/300/300"
+          />
+        </div>
+        <div class="xl:block hidden xl:w-1/3">
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Bad Life"
+            by="Sigrid"
+            to="/artist"
+            image="https://picsum.photos/id/211/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Dystopia"
+            by="Megadeth"
+            to="/artist"
+            image="https://picsum.photos/id/123/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Can You Feel My Heart"
+            by="Bring Me the Horizon"
+            to="/artist"
+            image="https://picsum.photos/id/94/300/300"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="px-8 mt-8 min-w-[800px]">
+      <div class="text-white text-xl font-semibold inline-block">
+        Made for you
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex justify-start gap-7"></div>
+    </div>
+
+    <div class="px-8 mt-8 min-w-[800px]">
+      <div class="py-3"></div>
+
+      <p class="text-center pb-6 text-[#D1D1D6] text-sm">
+        Unlock Flow by selecting your favourite artists for improved
+        recommendations.
+      </p>
+
+      <div class="flex justify-center gap-7 full">
+        <div class="flex items-center justify-center relative">
+          <img
+            width="100"
+            class="absolute -left-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/188/300/300"
+          />
+          <img
+            width="115"
+            class="absolute -left-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/66/300/300"
+          />
+          <img
+            width="160"
+            class="rounded-full z-20 cursor-pointer"
+            src="https://picsum.photos/id/144/300/300"
+          />
+          <img
+            width="115"
+            class="absolute -right-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/141/300/300"
+          />
+          <img
+            width="100"
+            class="absolute -right-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/222/300/300"
+          />
+        </div>
+      </div>
+      <div class="w-full flex justify-center p-3">
+        <button
+          type="button"
+          class="text-white text-[15px] font-semibold rounded-full bg-[#EF5465] hover:bg-[#d8384a] px-8 py-2 text-center"
+        >
+          ADD ARTISTS
+        </button>
+      </div>
+    </div>
+
+    <div class="mt-8 min-w-[800px]"></div>
+
+    <div class="pb-40"></div>
+  </div>
+</template>
